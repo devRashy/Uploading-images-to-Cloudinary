1dbfd8f64b99a39e1b7f0042c3fe77183300e692
yhuxuf <yhuxuf@gmail.com>
2020-02-07 16:34:17 +0100

    final commit

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-const express = require('express')
-const app = express()
-const cloudinary = require('cloudinary').v2
-const bodyParser = require('body parser')
+const express = require("express");
+const app = express();
+const cloudinary = require("cloudinary").v2;
+const bodyParser = require('body-parser');
+require('dotenv').config()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+// body parser configuration
+app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// cloudinary configuration
 cloudinary.config({
-    cloud_name: dy5bs7nmh,
-    API_KEY: 448396895414434,
-    API_SECRET: YaDxuVrdu-esJJp5aY8BkU99eLA
-}) 
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
+app.get("/", (req, res, next) => {
+  res.json({ message: "Hey! This is your server response!" });
+  next();
+});
 
-api.get('/', (req,res) => {
-    res.json({
-        message: "Hello world"
-    })
-})
+// image upload API
+app.post("/image-upload", (req, res) => {
+    // collected image from a user
+    const data = {
+      image: request.body.image,
+    }
 
-api.post('image_upload', (req,res) => {
+    // upload image here
+    cloudinary.uploader.upload(data.image)
+    .then((result) => {
+      res.status(200).send({
+        message: "success",
+        result,
+      });
+    }).catch((error) => {
+      res.status(500).send({
+        message: "failure",
+        error,
+      });
+    });
+});
 
+app.listen(4000,() => {
+    console.log('your app is running on port 4000')
 })
